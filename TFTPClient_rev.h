ac1072f6154f035ea29fd8200c0a2f91b59c4795
obarthel <obarthel@gmx.net>
2018-03-11 12:47:28 +0100

    Version bump

--- TFTPClient_rev.h
+++ TFTPClient_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	24
-#define DATE		"10.3.2018"
-#define VERS		"TFTPClient 1.24"
-#define VSTRING		"TFTPClient 1.24 (10.3.2018)\r\n"
-#define VERSTAG		"\0$VER: TFTPClient 1.24 (10.3.2018)"
+#define REVISION	25
+#define DATE		"11.3.2018"
+#define VERS		"TFTPClient 1.25"
+#define VSTRING		"TFTPClient 1.25 (11.3.2018)\r\n"
+#define VERSTAG		"\0$VER: TFTPClient 1.25 (11.3.2018)"
